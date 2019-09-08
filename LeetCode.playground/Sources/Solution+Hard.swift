724af4676155f4e101caaa46e6d9bfc1ab5ce732
zhiwei <zw@zwmpb.local>
2019-09-08 18:28:15 +0800

    hard, 97. Interleaving String, 2d dp

--- LeetCode.playground/Sources/Solution+Hard.swift
+++ LeetCode.playground/Sources/Solution+Hard.swift
@@ -98,6 +98,39 @@ public extension Solution{
     }
 
     /**
+     97. Interleaving String
+     */
+    func isInterleave(_ s1: String, _ s2: String, _ s3: String) -> Bool {
+        let s1 = Array(s1)
+        let s2 = Array(s2)
+        let s3 = Array(s3)
+        guard s1.count + s2.count == s3.count else { return false }
+        
+        var dp = Array(repeating: Array(repeating: false, count: s2.count+1), count: s1.count+1)
+        
+        //compute
+        for i in 1...s1.count {
+            for j in 1...s2.count{
+                if i == 0, j == 0{
+                    dp[i][j] = true
+                }
+                else if i == 0 {
+                    dp[i][j] = dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]
+                }
+                else if j == 0 {
+                    dp[i][j] = dp[i - 1][j] && s1[i - 1] == s3[j + i - 1]
+                }
+                else{
+                    dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1])
+                        || (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1])
+                }
+            }
+        }
+        return dp[s1.count][s2.count]
+    }
+
+
+    /**
      145. Binary Tree Postorder Traversal
      */
     func postorderTraversal(_ root: TreeNode?) -> [Int] {
